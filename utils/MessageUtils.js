2a3b03e9a93b8b765962219ec5d1ec374b75a04c
quangChe <quang.n.che@gmail.com>
2019-03-09 15:43:41 -0800

    Message Utility Functions
    Added utility functions to provide data model for messages, generate ID's for each message, and creat specific types of messages such as 'text', 'image' and 'coordinates'.

---
+++ utils/MessageUtils.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+export const MessageShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  type: PropTypes.oneOf(['text', 'image', 'location']),
+  text: PropTypes.string,
+  uri: PropTypes.string,
+  coordinate: PropTypes.shape({
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired
+  })
+})
+
+let messageId = 0;
+
+const getNextId = () => messageId += 1;
+
+export function createTextMessage(text) {
+  return {
+    type: 'text',
+    id: getNextId(),
+    text
+  }
+}
+
+export function createImageMessage(uri) {
+  return {
+    type: 'image',
+    id: getNextId(),
+    uri
+  }
+}
+
+export function createLocationmessage(coordinate) {
+  return {
+    type: 'location',
+    id: getNextId(),
+    coordinate
+  }
+}
